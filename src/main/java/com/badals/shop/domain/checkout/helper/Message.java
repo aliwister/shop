b3b6a0c8f12b9786730880e8b18fb0d59c3a12a5
Ali Allawati <ali@badals.com>
2020-03-24 20:12:32 +0400

    don't allow duplicate pricing requests

--- src/main/java/com/badals/shop/domain/checkout/helper/Message.java
+++ src/main/java/com/badals/shop/domain/checkout/helper/Message.java
@@ -5,8 +5,14 @@ import lombok.Data;
 @Data
 public class Message {
     String value;
+    String status;
 
     public Message(String value) {
         this.value = value;
+    }
+
+    public Message(String value, String status) {
+        this.value = value;
+        this.status = status;
     }
 }
