4cb781f3248e9772570ee32764eabfecdaff9a0e
Ali Allawati <ali@badals.com>
2020-03-31 21:27:20 +0400

    implement caching

--- src/main/java/com/badals/shop/domain/pojo/ProductResponse.java
+++ src/main/java/com/badals/shop/domain/pojo/ProductResponse.java
@@ -3,10 +3,11 @@ package com.badals.shop.domain.pojo;
 import com.badals.shop.service.dto.ProductDTO;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class ProductResponse {
+public class ProductResponse implements Serializable {
     List<ProductDTO> items;
     int total;
     boolean hasMore;
