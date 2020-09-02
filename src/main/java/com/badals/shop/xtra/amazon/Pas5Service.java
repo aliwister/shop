494536e662519e7e127ad27d4842a577b5c37062
Ali Allawati <ali@badals.com>
2020-09-03 01:15:02 +0400

    set inStock to true when offers available

--- src/main/java/com/badals/shop/xtra/amazon/Pas5Service.java
+++ src/main/java/com/badals/shop/xtra/amazon/Pas5Service.java
@@ -407,6 +407,7 @@ public class Pas5Service implements IProductService {
         Product product = p;
         try {
             product = setMerchantStock(p, MwsLookupParser.parseStock(getMerchantStock(p),n, p.getWeight(), overrides), BigDecimal.valueOf(99L));
+            product.inStock(true);
         } catch (PricingException e) {
             product.setPrice((BigDecimal) null);
             //e.printStackTrace();
