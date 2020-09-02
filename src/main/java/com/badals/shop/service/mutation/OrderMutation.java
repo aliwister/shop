3beb14062d4a16dd0c764b31c3441ae31e19d2d7
Ali Allawati <ali@badals.com>
2020-09-02 23:38:41 +0400

    close purchase/order

--- src/main/java/com/badals/shop/service/mutation/OrderMutation.java
+++ src/main/java/com/badals/shop/service/mutation/OrderMutation.java
@@ -2,9 +2,11 @@ package com.badals.shop.service.mutation;
 
 import com.badals.shop.service.OrderService;
 import com.badals.shop.service.PaymentService;
+import com.badals.shop.service.PurchaseService;
 import com.badals.shop.service.dto.OrderDTO;
 import com.badals.shop.service.dto.OrderItemDTO;
 import com.badals.shop.service.dto.PaymentDTO;
+import com.badals.shop.service.dto.PurchaseDTO;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
@@ -26,10 +28,12 @@ mutation {
 public class OrderMutation implements GraphQLMutationResolver {
 
     final private OrderService orderService;
+    final private PurchaseService purchaseService;
     final private PaymentService paymentService;
 
-    public OrderMutation(OrderService orderService, PaymentService paymentService) {
+    public OrderMutation(OrderService orderService, PurchaseService purchaseService, PaymentService paymentService) {
         this.orderService = orderService;
+        this.purchaseService = purchaseService;
         this.paymentService = paymentService;
     }
 
@@ -48,7 +52,25 @@ public class OrderMutation implements GraphQLMutationResolver {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public OrderDTO cancelOrder(Long id, String reason){
-        OrderDTO order = orderService.cancelOrder(id, reason);
+        OrderDTO order = orderService.cancel(id, reason);
+        return order;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public OrderDTO closeOrder(Long id, String reason){
+        OrderDTO order = orderService.close(id, reason);
+        return order;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public PurchaseDTO cancelPurchase(Long id, String reason){
+        PurchaseDTO order = purchaseService.cancel(id, reason);
+        return order;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public PurchaseDTO closePurchase(Long id, String reason){
+        PurchaseDTO order = purchaseService.close(id, reason);
         return order;
     }
 
