39d9fc0a51211da136c2ad05d882b041ccf611f6
Ali Allawati <ali@badals.com>
2020-09-10 12:45:04 +0400

    fix email (no account number info when checkout) fix pricing (default to mws when no override)

--- src/main/java/com/badals/shop/xtra/IProductService.java
+++ src/main/java/com/badals/shop/xtra/IProductService.java
@@ -3,7 +3,8 @@ package com.badals.shop.xtra;
 import com.badals.shop.domain.Product;
 import com.badals.shop.service.dto.ProductDTO;
 import com.badals.shop.xtra.amazon.NoOfferException;
+import com.badals.shop.xtra.amazon.PricingException;
 
 public interface IProductService {
-    Product lookup(String sku, boolean isParent, boolean isRedis, boolean isRebuild, boolean customFlag) throws NoOfferException;
+    Product lookup(String sku, boolean isParent, boolean isRedis, boolean isRebuild, boolean customFlag) throws NoOfferException, PricingException;
 }
