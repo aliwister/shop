4ba9fb7b5db6560033fc633f15753eec8481546a
Ali Allawati <ali@badals.com>
2021-01-04 08:32:04 +0400

    fix no address customer

--- src/main/java/com/badals/shop/repository/CustomerRepository.java
+++ src/main/java/com/badals/shop/repository/CustomerRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -17,8 +18,8 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
    public Optional<Customer> findByEmailIgnoreCase(String email);
 
-   @Query("from Customer c join fetch c.addresses where c.mobile = ?1")
-   public Optional<Customer> findByMobileJoinAddresses(String mobile);
+   @Query("from Customer c left join fetch c.addresses where c.mobile = ?1")
+   public List<Customer> findByMobileJoinAddresses(String mobile);
 
    @Query("from Customer c left join fetch c.addresses where c.id = ?1")
    public Optional<Customer> findByIdJoinAddresses(Long id);
