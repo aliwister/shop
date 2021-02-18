2dd2c68fa71dd6d908bbd9aad25c2b1e411a4ef5
Ali Wister <ali@badals.com>
2021-02-18 14:17:17 +0400

    partner initial commit
    Signed-off-by: Ali Wister <ali@badals.com>

--- src/main/java/com/badals/shop/service/query/PartnerQuery.java
+++ src/main/java/com/badals/shop/service/query/PartnerQuery.java
@@ -1,11 +1,10 @@
 package com.badals.shop.service.query;
 
-import com.badals.shop.aop.logging.TenantContext;
-import com.badals.shop.domain.pojo.MerchantProductResponse;
 import com.badals.shop.service.CategoryService;
+import com.badals.shop.service.PartnerService;
 import com.badals.shop.service.ProductService;
 import com.badals.shop.service.UserService;
-import com.badals.shop.service.pojo.PartnerProductDTO;
+import com.badals.shop.service.pojo.PartnerProduct;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,22 +14,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class PartnerQuery extends ShopQuery implements GraphQLQueryResolver {
 
-   private final ProductService productService;
+   private final PartnerService partnerService;
 
    private final CategoryService categoryService;
    private static final Logger log = LoggerFactory.getLogger(PartnerQuery.class);
 
    private final UserService userService;
 
-   public PartnerQuery(ProductService productService, CategoryService categoryService, UserService userService) {
-      this.productService = productService;
+   public PartnerQuery(PartnerService partnerService, CategoryService categoryService, UserService userService) {
+      this.partnerService = partnerService;
       this.categoryService = categoryService;
       this.userService = userService;
    }
 
-   @PreAuthorize("hasRole('ROLE_MERCHANT')")
-    public PartnerProductDTO partnerProduct(Long id) {
-       return null;
+   //@PreAuthorize("hasRole('ROLE_MERCHANT')")
+    public PartnerProduct partnerProduct(Long id) {
+       Long mId = 1L;//TenantContext.getCurrentMerchantId();
+       return partnerService.getPartnerProduct(id, mId);
     }
 
 
