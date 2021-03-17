8bbec34524688a8727814817c7989c3581374e61
Ali Wister <ali@badals.com>
2021-03-17 13:00:38 +0400

    balance search no development built --skip-ci

--- src/main/java/com/badals/shop/repository/search/OrderSearchRepository.java
+++ src/main/java/com/badals/shop/repository/search/OrderSearchRepository.java
@@ -1,8 +1,18 @@
 package com.badals.shop.repository.search;
 
+import com.badals.shop.domain.enumeration.OrderState;
 import com.badals.shop.service.dto.OrderDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface OrderSearchRepository extends ElasticsearchRepository<OrderDTO, Long> {
    //AddProductDTO findBySlug(String slug);
+
+   List<OrderDTO> findAllByCustomerContainsOrderById(String search, Pageable p);
+
+   List<OrderDTO> findAllByOrderStateInAndBalanceNot(List<OrderState> orderStates, BigDecimal balance, Pageable p);
 }
