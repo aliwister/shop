88c18b4d120ce4b1f71ff1cffe780c371a4e2172
Ali Wister <ali@badals.com>
2021-05-10 19:40:43 +0400

    fix voided null -> false

--- src/main/java/com/badals/shop/repository/PaymentRepository.java
+++ src/main/java/com/badals/shop/repository/PaymentRepository.java
@@ -22,7 +22,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
    @Query("from Payment p where p.order.id = ?1")
    List<Payment> findAllByOrderId(Long orderId);
 
-   @Query("select p from Payment p left join p.order o left join o.customer c where p.paymentMethod in (?1) and (?2 is null or p.settlementDate >= ?2) and (?3 is null or p.settlementDate <= ?3) and (?4 is null or p.order is null or p.order.customer.id = ?4 ) and (?5 is null or p.account = ?5 ) and (?6 is null or p.amount < ?6 ) and (?7 = false or p.settlementDate is null) and p.voided is null order by p.id DESC")
+   @Query("select p from Payment p left join p.order o left join o.customer c where p.paymentMethod in (?1) and (?2 is null or p.settlementDate >= ?2) and (?3 is null or p.settlementDate <= ?3) and (?4 is null or p.order is null or p.order.customer.id = ?4 ) and (?5 is null or p.account = ?5 ) and (?6 is null or p.amount < ?6 ) and (?7 = false or p.settlementDate is null) and p.voided = false order by p.id DESC")
    Page<Payment> findForTable(List<String> paymentMethods, Date from, Date to, Long customerId, String accountCode, String maxAmount, Boolean unsettledOnly , PageRequest of);
 
    @Modifying @Query("update Payment p set p.settlementDate = ?2 where p.id in ?1")
