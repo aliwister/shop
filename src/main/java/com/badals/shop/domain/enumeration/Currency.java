d4a8542faf9c9bb4215fe68595ed10b1ae75366f
Ali Wister <ali@badals.com>
2021-05-12 01:08:57 +0400

    add currencies query

--- src/main/java/com/badals/shop/domain/enumeration/Currency.java
+++ src/main/java/com/badals/shop/domain/enumeration/Currency.java
@@ -1,15 +1,24 @@
 package com.badals.shop.domain.enumeration;
 
+import java.io.Serializable;
+
 /**
  * The State enumeration.
  */
-public enum Currency {
-    OMR,
-    AED,
-    SAR,
-    KWD,
-    BHD,
-    QAR,
-    USD,
-    EUR
+
+public enum Currency implements Serializable {
+    OMR("OMR"),
+    AED("AED"),
+    SAR("SAR"),
+    KWD("KWD"),
+    BHD("BHD"),
+    QAR("QAR"),
+    USD("USD"),
+    EUR("EUR");
+
+    private String code;
+
+    Currency(String code) {
+        this.code = code;
+    }
 }
