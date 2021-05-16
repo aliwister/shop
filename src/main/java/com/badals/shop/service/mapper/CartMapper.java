3de5aaa3231d4981eee61f70a5e40fe296553633
Ali Wister <ali@badals.com>
2021-05-16 13:14:08 +0400

    remove wrong products

--- src/main/java/com/badals/shop/service/mapper/CartMapper.java
+++ src/main/java/com/badals/shop/service/mapper/CartMapper.java
@@ -1,9 +1,18 @@
 package com.badals.shop.service.mapper;
 
 import com.badals.shop.domain.*;
+import com.badals.shop.domain.pojo.Gallery;
 import com.badals.shop.service.dto.CartDTO;
 
+import com.badals.shop.service.dto.CartItemDTO;
+import com.badals.shop.service.dto.ProductDTO;
 import org.mapstruct.*;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Mapper for the entity {@link Cart} and its DTO {@link CartDTO}.
@@ -35,4 +44,12 @@ public interface CartMapper extends EntityMapper<CartDTO, Cart> {
         cart.setId(id);
         return cart;
     }
+
+    @AfterMapping
+    default void afterMapping(@MappingTarget CartDTO target, Cart source) {
+        List<CartItemDTO> items = target.getCartItems();
+        items = items.stream().filter(x -> x.getProductId() != null).collect(Collectors.toList());
+        target.setCartItems(items);
+    }
+
 }
