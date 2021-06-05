f59ee9855a3d5443d753c604ef77d89441341631
Ali Wister <ali@badals.com>
2021-06-05 15:53:03 +0400

    fix translation

--- src/main/java/com/badals/shop/aop/logging/LocaleAspect.java
+++ src/main/java/com/badals/shop/aop/logging/LocaleAspect.java
@@ -17,7 +17,8 @@ import java.util.Optional;
 @Component
 public class LocaleAspect {
 
-    @Around("execution(* com.badals.shop.graph.query.ProductQuery.product(..))")
+    @Around("execution(* com.badals.shop.graph.query.ProductQuery.product(..)) || "+
+            "execution(* com.badals.shop.graph.query.ProductQuery.getProductBySku(..))")
     public Object beforeWebMethodExecution1(ProceedingJoinPoint joinPoint) throws Throwable {
         setLocale(joinPoint);
         return joinPoint.proceed();
