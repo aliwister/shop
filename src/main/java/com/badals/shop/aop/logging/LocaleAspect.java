9f296e7d656283b6e599b8f755d8f02ef3ed7627
Ali Wister <ali@badals.com>
2021-06-05 19:01:58 +0400

    update maximum pool size

--- src/main/java/com/badals/shop/aop/logging/LocaleAspect.java
+++ src/main/java/com/badals/shop/aop/logging/LocaleAspect.java
@@ -36,8 +36,6 @@ public class LocaleAspect {
                 String key, value;
                 key = codeSignature.getParameterNames()[i];
                 value = joinPoint == null || joinPoint.getArgs() == null || joinPoint.getArgs()[i] == null?"":joinPoint.getArgs()[i].toString();
-                System.out.println("First parameter's name: " + key);
-                System.out.println("First argument's value: " + value);
 
                 if (key.equalsIgnoreCase("_locale"))
                     LocaleContext.setLocale(value);
