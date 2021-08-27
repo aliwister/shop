e56657918f42be1f884eb6e5031442f7c02993ac
Ali Wister <ali@badals.com>
2021-08-27 10:48:38 +0400

    updates for checkout improvement tenant info

--- src/main/java/com/badals/shop/graph/query/FieldDescription.java
+++ src/main/java/com/badals/shop/graph/query/FieldDescription.java
@@ -8,17 +8,21 @@ import java.util.List;
 public class FieldDescription {
    AddressField field;
    String label;
-   Boolean isRequired;
+   Boolean required;
    String regex;
    FieldType fieldType;
    List<Option> options;
+   final Integer minLength;
+   final Integer maxLength;
 
-   public FieldDescription(AddressField field, String label, Boolean isRequired, String regex, FieldType fieldType, List<Option> options) {
+   public FieldDescription(AddressField field, String label, Boolean required, String regex, FieldType fieldType, List<Option> options, Integer minLength, Integer maxLength) {
       this.field = field;
       this.label = label;
-      this.isRequired = isRequired;
+      this.required = required;
       this.regex = regex;
       this.fieldType = fieldType;
       this.options = options;
+      this.minLength = minLength;
+      this.maxLength = maxLength;
    }
 }
