3dba20b6f15fae239aa66a474f09a73ce7a3f719
Ali Wister <ali@badals.com>
2021-12-01 19:18:43 +0400

    add length to purchase

--- src/main/java/com/badals/shop/service/mapper/PurchaseMapper.java
+++ src/main/java/com/badals/shop/service/mapper/PurchaseMapper.java
@@ -1,6 +1,7 @@
 package com.badals.shop.service.mapper;
 
 import com.badals.shop.domain.*;
+import com.badals.shop.service.dto.ProductDTO;
 import com.badals.shop.service.dto.PurchaseDTO;
 
 import org.mapstruct.*;
@@ -31,6 +32,11 @@ public interface PurchaseMapper extends EntityMapper<PurchaseDTO, Purchase> {
     @Mapping(source = "merchant", target = "merchantObj")
     @Mapping(target="purchaseItems", ignore = true)
     PurchaseDTO toDtoList(Purchase purchase);
+
+    @AfterMapping
+    default void afterMapping(@MappingTarget PurchaseDTO target, Purchase source) {
+        target.setLength(source.getPurchaseItems().size());
+    }
 
     default Purchase fromId(Long id) {
         if (id == null) {
