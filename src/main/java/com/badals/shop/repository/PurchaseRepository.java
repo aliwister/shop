19b78f66c6007c993af5b04bd59fcfdfaa013d0f
Ali <ali@badals.com>
2022-11-04 16:07:22 -0400

    merge main

--- src/main/java/com/badals/shop/repository/PurchaseRepository.java
+++ src/main/java/com/badals/shop/repository/PurchaseRepository.java
@@ -1,6 +1,5 @@
 package com.badals.shop.repository;
 
-import com.badals.shop.domain.Order;
 import com.badals.shop.domain.Purchase;
 import com.badals.shop.repository.projection.CartItemInfo;
 import com.badals.shop.repository.projection.PurchaseQueue;
@@ -32,14 +31,50 @@ public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
    @Query("from Purchase p left join fetch p.merchant left join fetch p.deliveryAddress left join fetch p.purchaseItems i left join fetch i.orderItems where p.id = ?1 order by i.sequence")
    Optional<Purchase> findForPurchaseDetails(Long id);
 
-   @Query(value="Select id, product_name as productName, outstanding as quantity, image, weight, price, url, sku, cost, order_id as orderId, product_id as productId, attributes, merchantId from purchase_queue", nativeQuery=true)
+   @Query(value="select  " +
+           "    `o`.`id` as `oid`,  " +
+           "    `oi`.`id` as `id`,  " +
+           "    `oi`.`product_name` as `productName`,  " +
+           "    `oi`.`quantity` as `orderQuantity`,  " +
+           "    `oi`.`price` as `price`,  " +
+           "    `oi`.`comment` as `comment`,  " +
+           "    `oi`.`sequence` as `sequence`,  " +
+           "    `oi`.`shipping_instructions` as `shipping_instructions`,  " +
+           "    `oi`.`order_id` as `orderId`,  " +
+           "    `oi`.`image` as `image`,  " +
+           "    `oi`.`unit` as `unit`,  " +
+           "    `oi`.`weight` as `weight`,  " +
+           "    `oi`.`line_total` as `line_total`,  " +
+           "    ifnull(`oi`.`url`,(select `pp`.`url` from `profileshop`.`product` `pp` where (`pp`.`ref` = `oi`.`product_id`))) as `url`,  " +
+           "    (select `pp`.`merchant_id` from `profileshop`.`product` `pp` where (`pp`.`ref` = `oi`.`product_id`)) as `merchantId`,  " +
+           "    `oi`.`sku` as `sku`,  " +
+           "    `oi`.`product_id` as `productId`,  " +
+           "    ifnull(sum(`pui`.`quantity`), 0) as `purchased`,  " +
+           "    (`oi`.`quantity` - ifnull(sum(`pui`.`quantity`), 0)) as `quantity`,  " +
+           "    ifnull((select sum(`os`.`quantity`) from ((`admin`.`order_shipment` `os` join `admin`.`shipment_item` `si` on  " +
+           "        ((`os`.`shipment_item_id` = `si`.`id`))) join `admin`.`shipment` `s` on ((`s`.`id` = `si`.`shipment_id`)))  " +
+           "    where ((`os`.`order_item_id` = `oi`.`id`)  and (`s`.`shipment_type` = 'CUSTOMER')) group by `os`.`order_item_id`),0) as `issued`  " +
+           "from `profileshop`.`order_item` `oi`  " +
+           "join `profileshop`.`jhi_order` `o` on `o`.`id` = `oi`.`order_id` and o.state in ('PAYMENT_ACCEPTED', 'DELIVERED')  " +
+           "left join `profileshop`.`purchase_item_order_item` `pioi` on `pioi`.`order_item_id` = `oi`.`id`  " +
+           "left join `profileshop`.`purchase_item` `pui` on `pui`.`id` = `pioi`.`purchase_item_id`  " +
+           "where  " +
+           "     o.tenant_id = 'badals'  " +
+           "     and o.id> 9000  " +
+           "group by  " +
+           "    `oi`.`id`  " +
+           "having  " +
+           "    `oi`.`quantity` > `purchased` and `issued` < `quantity`  " +
+           "order by  " +
+           "    `oi`.`id` desc", nativeQuery=true)
    List<PurchaseQueue> getPurchaseQueue();
 
-   @Query(value="SELECT pit.description as productName, pit.quantity - ifnull(SUM(si.quantity),0) as quantity, pp.image, pp.weight, pp.price, pp.url, pp.sku FROM shop.purchase_item pit  " +
-           "left JOIN shop.purchase p ON p.id = pit.purchase_id  " +
+
+   @Query(value="SELECT pit.description as productName, pit.quantity - ifnull(SUM(si.quantity),0) as quantity, pp.image, pp.weight, pp.price, pp.url, pp.sku FROM profileshop.purchase_item pit  " +
+           "left JOIN profileshop.purchase p ON p.id = pit.purchase_id  " +
            "left JOIN admin.purchase_shipment ps ON ps.purchase_item_id = pit.id  " +
            "left JOIN admin.shipment_item si ON ps.shipment_item_id = si.id  " +
-           "LEFT JOIN shop.product pp ON pp.ref = pit.product_id  " +
+           "LEFT JOIN profileshop.product pp ON pp.ref = pit.product_id  " +
            "WHERE p.order_state <> 'CANCELED' OR p.order_state IS null  " +
            "GROUP BY pit.id  " +
            "HAVING quantity > 0  ", nativeQuery=true)
