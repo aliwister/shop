a7788b5fde84f58f68f9cfc37f6e75951c8cd0eb
Ali <ali@badals.com>
2022-11-10 14:58:19 -0500

    add /partner-me end point to retrieve tenant list

--- src/main/java/com/badals/shop/config/SecurityConfiguration.java
+++ src/main/java/com/badals/shop/config/SecurityConfiguration.java
@@ -95,6 +95,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/authenticate").permitAll()
                     .antMatchers("/api/authenticate2").permitAll()
                     .antMatchers("/api/store-select").permitAll()
+                    .antMatchers("/api/partner-me").authenticated()
                     .antMatchers("/api/register").permitAll()
                     .antMatchers("/api/activate").permitAll()
                     .antMatchers("/api/product/**").permitAll()
