4104ab5eb5fe16ea0cf818a0176d34160e7f3bb9
Ali <ali@badals.com>
2023-07-04 23:31:48 +0200

    fix store-select

--- src/main/java/com/badals/shop/security/jwt/ProfileUser.java
+++ src/main/java/com/badals/shop/security/jwt/ProfileUser.java
@@ -5,13 +5,13 @@ import lombok.Setter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 
 import java.util.Collection;
 
 public class ProfileUser extends User {
-   public ProfileUser(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+   public ProfileUser(String username, String password, Collection<? extends GrantedAuthority> authorities, String issuer) {
       super(username, password, authorities);
+      this.isStoreSelect = issuer != null && issuer.equals("true");
    }
 
    public ProfileUser(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
@@ -28,6 +28,10 @@ public class ProfileUser extends User {
    @Getter
    @Setter
    String profile;
+
+   @Getter
+   @Setter
+   boolean isStoreSelect;
 
    public boolean hasAuthority(Authentication authentication, String authority) {
       return authentication.getAuthorities().stream()
