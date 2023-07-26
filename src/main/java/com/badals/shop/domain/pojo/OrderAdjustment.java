28bd8aae522c72384e4ef333bfca3746283080a7
Ali <ali@badals.com>
2023-07-26 15:34:25 +0200

    add update carrier

--- src/main/java/com/badals/shop/domain/pojo/OrderAdjustment.java
+++ src/main/java/com/badals/shop/domain/pojo/OrderAdjustment.java
@@ -19,6 +19,13 @@ public class OrderAdjustment implements Serializable {
    public OrderAdjustment() {
    }
 
+   public void setShippingValueAndType(BigDecimal value, String description) {
+      this.value = value;
+      this.description = description;
+      this.type = OrderAdjustmentType.SHIPPING;
+      this.quantity = 1;
+   }
+
    public OrderAdjustment(OrderAdjustmentType type, String description, Integer quantity, BigDecimal value) {
       this.type = type;
       this.description = description;
@@ -30,5 +37,9 @@ public class OrderAdjustment implements Serializable {
    }
    public static OrderAdjustment delivery(String description, BigDecimal value) {
       return new OrderAdjustment(OrderAdjustmentType.SHIPPING, description, 1, value);
+   }
+
+   public boolean shippingCheck() {
+      return this.getType().equals(OrderAdjustmentType.SHIPPING);
    }
 }
