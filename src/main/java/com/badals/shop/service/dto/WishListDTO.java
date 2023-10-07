6812590413c6b7409d128a0741694a9993c2d58d
Ebi <tmpmailebi@gmail.com>
2023-10-08 00:02:42 +0330

    created TenantWishListService and WishListDTO

---
+++ src/main/java/com/badals/shop/service/dto/WishListDTO.java
@@ -0,0 +1,40 @@
+package com.badals.shop.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+public class WishListDTO implements Serializable {
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        WishListDTO wishlistDTO = (WishListDTO) o;
+        if ("wishlistDTO.getId()" == null || "getId()" == null) {
+            return false;
+        }
+//        return Objects.equals(getId(), cartDTO.getId());
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(1234);
+    }
+
+    @Override
+    public String toString() {
+        return "WishListDTO{";
+    }
+}
