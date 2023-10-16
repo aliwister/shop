9b78dcb2a4752dcdd0ab5d62e7da46e09e32c707
Ebi <tmpmailebi@gmail.com>
2023-10-16 23:30:38 +0330

    added pageinfo repository

---
+++ src/main/java/com/badals/shop/repository/PageInfoRepository.java
@@ -0,0 +1,11 @@
+package com.badals.shop.repository;
+
+import com.badals.shop.domain.tenant.PageInfo;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PageInfoRepository extends CrudRepository<PageInfo, Long> {
+    List<PageInfo> findPageInfosBySlugAndTenantId(String slug, String tenantId);
+    PageInfo findPageInfoBySlugAndTenantIdAndLanguage(String slug, String tenantId, String language);
+}
