383e924889e244a134746a6457516681c611de67
Ebi <tmpmailebi@gmail.com>
2023-10-21 20:10:18 +0330

    added service for page and pageInfo crud

--- src/main/java/com/badals/shop/service/PageInfoService.java
+++ src/main/java/com/badals/shop/service/PageInfoService.java
@@ -1,75 +1,73 @@
 package com.badals.shop.service;
 
 import com.badals.shop.aop.tenant.TenantContext;
+import com.badals.shop.domain.tenant.Page;
 import com.badals.shop.domain.tenant.PageInfo;
 import com.badals.shop.repository.PageInfoRepository;
-import com.badals.shop.service.dto.PageInfoDTO;
-import com.badals.shop.service.dto.PagesInfosDTO;
+import com.badals.shop.repository.PageRepository;
+import com.badals.shop.service.dto.PageInfoInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 @Service
 @Transactional
 public class PageInfoService {
     private final Logger log = LoggerFactory.getLogger(PageInfoService.class);
     private final PageInfoRepository pageInfoRepository;
-
-    public PageInfoService(PageInfoRepository pageInfoRepository) {
+    private final PageRepository pageRepository;
+    public PageInfoService(PageInfoRepository pageInfoRepository, PageRepository pageRepository) {
         this.pageInfoRepository = pageInfoRepository;
+        this.pageRepository = pageRepository;
     }
 
-    public PageInfo createPageInfo(PageInfoDTO pageInfoDTO) {
+    public Page createPageInfo(PageInfoInput pageInfoInput) {
+        Page page = pageRepository.findPageBySlugAndTenantId(pageInfoInput.getSlug(),  TenantContext.getCurrentProfile());
+        if (page == null){
+            Page new_page = new Page();
+            new_page.setSlug(pageInfoInput.getSlug());
+            new_page.setIsImportant(pageInfoInput.getIsImportant());
+            page = pageRepository.save(new_page);
+        }
         PageInfo pageInfo = new PageInfo();
-        pageInfo.setLanguage(pageInfoDTO.getLanguage());
-        pageInfo.setSlug(pageInfoDTO.getSlug());
-        pageInfo.setInfo(pageInfoDTO.getInfo());
-        return pageInfoRepository.save(pageInfo);
+        pageInfo.setLanguage(pageInfoInput.getLanguage());
+        pageInfo.setInfo(pageInfoInput.getInfo());
+        pageInfo.setPage(page);
+        page.getPageInfos().add(pageInfo);
+                    return pageRepository.save(page);
     }
 
     public PageInfo getPageInfoById(Long id) {
         return pageInfoRepository.findById(id).orElse(null);
     }
-    public List<PageInfo> getPageInfosBySlugAndAndTenantId(String slug, String tenant_id) {
-        return pageInfoRepository.findPageInfosBySlugAndTenantId(slug, tenant_id);
+    public Page getPageInfosBySlugAndAndTenantId(String slug, String tenant_id) {
+        return pageRepository.findPageBySlugAndTenantId(slug, tenant_id);
     }
 
-    public PageInfo updatePageInfo(PageInfoDTO pageInfoDTO) throws Exception {
-        PageInfo pageInfo = pageInfoRepository.findPageInfoBySlugAndTenantIdAndLanguage(pageInfoDTO.getSlug(), TenantContext.getCurrentProfile(), pageInfoDTO.getLanguage());
+    public PageInfo updatePageInfo(PageInfoInput pageInfoInput) throws Exception {
+        PageInfo pageInfo = pageInfoRepository.findPageInfoBySlugAndTenantIdAndLanguage(pageInfoInput.getSlug(), TenantContext.getCurrentProfile(), pageInfoInput.getLanguage()).orElse(null);
         if (pageInfo == null)
             throw new Exception("Page info not found");
-        pageInfo.setInfo(pageInfoDTO.getInfo());
+        pageInfo.setInfo(pageInfoInput.getInfo());
         return pageInfoRepository.save(pageInfo);
     }
 
-    public PageInfo getPageInfoBySlugAndTenantIdAndLanguage(String slug, String tenant_id, String language) {
-        return pageInfoRepository.findPageInfoBySlugAndTenantIdAndLanguage(slug,tenant_id, language);
+//    public PageInfo getPageInfoBySlugAndTenantIdAndLanguage(String slug, String tenant_id, String language) {
+//        return pageInfoRepository.findPageInfoBySlugAndTenantIdAndLanguage(slug,tenant_id, language);
+//    }
+
+    public List<Page> getPagesInfosByTenantID(String tenant_id) {
+        return pageRepository.findPagesByTenantId(tenant_id);
     }
 
-    public List<PagesInfosDTO> getPagesInfosByTenantID(String tenant_id) {
-        List<PageInfo> pageInfos = pageInfoRepository.findPageInfosByTenantId(tenant_id);
-        Map<String, PagesInfosDTO> map = new HashMap<>();
-        for (PageInfo pageInfo : pageInfos) {
-            if (map.containsKey(pageInfo.getSlug())) {
-                map.get(pageInfo.getSlug()).getPageInfos().add(pageInfo);
-            } else {
-                PagesInfosDTO pagesInfosDTO = new PagesInfosDTO();
-                pagesInfosDTO.setSlug(pageInfo.getSlug());
-                pagesInfosDTO.getPageInfos().add(pageInfo);
-                map.put(pageInfo.getSlug(), pagesInfosDTO);
-            }
-        }
-        return new ArrayList<>(map.values());
-    }
-
-    public void deleteById(Long id) {
-        pageInfoRepository.deleteById(id);
+    public void deleteByIdAndAndTenantId(PageInfo pageInfo, Long id, String tenant_id){
+        pageInfo.getPage().getPageInfos().removeIf(page -> Objects.equals(page.getId(), id));
+        pageRepository.save(pageInfo.getPage());
+        pageInfoRepository.deleteByIdAndAndTenantId(id, tenant_id);
     }
 
     public PageInfo save(PageInfo pageInfo) {
