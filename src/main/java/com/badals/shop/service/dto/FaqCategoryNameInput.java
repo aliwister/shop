3a29bda0dd6720e0d97c0965711b69471f4201a6
Ebi <tmpmailebi@gmail.com>
2023-10-22 23:48:27 +0330

    added input types for add mutations

---
+++ src/main/java/com/badals/shop/service/dto/FaqCategoryNameInput.java
@@ -0,0 +1,19 @@
+package com.badals.shop.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class FaqCategoryNameInput implements Serializable {
+
+    private Integer position;
+    @NotNull
+    private String name;
+    @NotNull
+    private String language;
+}
