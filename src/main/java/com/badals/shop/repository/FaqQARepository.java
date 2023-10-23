dd0e14eb98e4ff33df21a51ab18132e625946e43
Ebi <tmpmailebi@gmail.com>
2023-10-23 21:48:33 +0330

    removed redundant queries

--- src/main/java/com/badals/shop/repository/FaqQARepository.java
+++ src/main/java/com/badals/shop/repository/FaqQARepository.java
@@ -4,10 +4,7 @@ import com.badals.shop.domain.tenant.TenantFaqQA;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface FaqQARepository extends JpaRepository<TenantFaqQA,Long> {
-    List<TenantFaqQA> findAllByTenantId(String tenantId);
-    TenantFaqQA findTenantFaqQAByCategoryIdAndTenantIdAndPosition(Long categoryId, String tenantId, Integer position);
+    TenantFaqQA findTenantFaqQAByCategoryIdAndPosition(Long categoryId, Integer position);
 }
