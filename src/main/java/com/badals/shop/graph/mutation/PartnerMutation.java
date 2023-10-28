a97a0759035f383d5430d5ff680dcdaabe029387
Ebi <tmpmailebi@gmail.com>
2023-10-28 19:05:31 +0330

    added removePageby slug mutation and requested by mohammad

--- src/main/java/com/badals/shop/graph/mutation/PartnerMutation.java
+++ src/main/java/com/badals/shop/graph/mutation/PartnerMutation.java
@@ -239,6 +239,15 @@ public class PartnerMutation implements GraphQLMutationResolver {
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
+    public Message removePage(String slug){
+        Page page = pageInfoService.getPageBySlug(slug);
+        if (page == null)
+            return new Message("Page not found", "404");
+        pageInfoService.deletePage(page.getId());
+        return new Message("ok");
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
     public PageInfo updatePageInfo(PageInfoInput info) throws Exception {
         return pageInfoService.updatePageInfo(info);
     }
--- src/main/java/com/badals/shop/repository/PageRepository.java
+++ src/main/java/com/badals/shop/repository/PageRepository.java
@@ -9,4 +9,6 @@ public interface PageRepository extends JpaRepository<Page, Long> {
     Page findPageBySlug(String slug);
     Page findPageBySlugAndTenantId(String slug, String tenant_id);
     List<Page> findAllByTenantId(String tenant_id);
+
+
 }
