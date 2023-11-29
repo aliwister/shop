7281b6c31cd3c36f7c4a0da872c399977b9f5e14
Ebi <tmpmailebi@gmail.com>
2023-11-29 23:28:57 +0330

    added builders

--- src/main/java/com/badals/shop/domain/SignUpQuestion.java
+++ src/main/java/com/badals/shop/domain/SignUpQuestion.java
@@ -1,12 +1,20 @@
 package com.badals.shop.domain;
 
+import com.badals.shop.domain.pojo.SignUpQuestionDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "signup_question", catalog = "profileshop")
 public class SignUpQuestion implements Serializable {
 
