1196c14a4a4c2c97c1a810ea1e4db719264d649e
Ebi <tmpmailebi@gmail.com>
2023-12-11 21:29:27 +0330

    returning jwt token after register

--- src/main/java/com/badals/shop/web/rest/UserCustomerAccountResource.java
+++ src/main/java/com/badals/shop/web/rest/UserCustomerAccountResource.java
@@ -2,7 +2,12 @@ package com.badals.shop.web.rest;
 
 
 import com.badals.shop.domain.Customer;
+import com.badals.shop.domain.tenant.Tenant;
 import com.badals.shop.repository.CustomerRepository;
+import com.badals.shop.repository.TenantRepository;
+import com.badals.shop.repository.UserRepository;
+import com.badals.shop.security.jwt.JWTFilter;
+import com.badals.shop.security.jwt.TokenProvider;
 import com.badals.shop.service.CustomerService;
 import com.badals.shop.service.util.MailService;
 import com.badals.shop.service.dto.PasswordChangeDTO;
@@ -11,15 +16,26 @@ import com.badals.shop.web.rest.errors.*;
 import com.badals.shop.web.rest.vm.KeyAndPasswordVM;
 import com.badals.shop.web.rest.vm.ManagedUserVM;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * REST controller for managing the current user's account.
@@ -37,34 +53,55 @@ public class UserCustomerAccountResource {
     private final Logger log = LoggerFactory.getLogger(UserCustomerAccountResource.class);
 
     private final CustomerRepository userRepository;
-
+    private final UserRepository user2Repository;
     private final CustomerService userService;
+    private final TenantRepository tenantRepository;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     private final MailService mailService;
 
-    public UserCustomerAccountResource(CustomerRepository userRepository, CustomerService userService, MailService mailService) {
+    public UserCustomerAccountResource(CustomerRepository userRepository, CustomerService userService, MailService mailService, UserRepository user2Repository, TenantRepository tenantRepository, TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
 
         this.userRepository = userRepository;
         this.userService = userService;
         this.mailService = mailService;
+        this.user2Repository = user2Repository;
+        this.tenantRepository = tenantRepository;
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
     }
 
     /**
      * {@code POST  /register} : register the user.
      *
      * @param managedUserVM the managed user View Model.
-     * @throws InvalidPasswordException {@code 400 (Bad Request)} if the password is incorrect.
+     * @return
+     * @throws InvalidPasswordException  {@code 400 (Bad Request)} if the password is incorrect.
      * @throws EmailAlreadyUsedException {@code 400 (Bad Request)} if the email is already used.
      * @throws LoginAlreadyUsedException {@code 400 (Bad Request)} if the login is already used.
      */
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public void registerAccount(@Valid @RequestBody ManagedUserVM managedUserVM) {
+    public ResponseEntity<JwtPartnerAuthenticationResponse> registerAccount(@Valid @RequestBody ManagedUserVM managedUserVM) {
         if (!checkPasswordLength(managedUserVM.getPassword())) {
             throw new InvalidPasswordException();
         }
         Customer user = userService.registerUser(managedUserVM, managedUserVM.getPassword());
+        UsernamePasswordAuthenticationToken authenticationToken =
+            new UsernamePasswordAuthenticationToken(user.getEmail(), managedUserVM.getPassword());
+        com.badals.shop.domain.User dbUser = user2Repository.findOneByEmailIgnoreCaseAndTenantId(user.getEmail(), com.badals.shop.domain.User.tenantFilter).get();
+        List<Tenant> tenantList = tenantRepository.findTenantsForUser(dbUser.getId());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        boolean rememberMe = true;
+        String jwt = tokenProvider.createToken(authentication, rememberMe, true);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
         mailService.sendActivationEmail(user);
+        return new ResponseEntity<>(new UserCustomerAccountResource.JwtPartnerAuthenticationResponse(jwt, authentication.getPrincipal(), tenantList, true), httpHeaders, HttpStatus.OK);
+
     }
 
     /**
@@ -180,5 +217,27 @@ public class UserCustomerAccountResource {
         return !StringUtils.isEmpty(password) &&
             password.length() >= ManagedUserVM.PASSWORD_MIN_LENGTH &&
             password.length() <= ManagedUserVM.PASSWORD_MAX_LENGTH;
+    }
+
+    @Data
+    static class JwtPartnerAuthenticationResponse {
+        private final String authorities;
+        private final String tenants;
+        @JsonProperty("id_token")
+        private final String idToken;
+        private final Boolean isStoreSelect;
+        private final String tokenType = "Bearer";
+        private final String username;
+
+        public JwtPartnerAuthenticationResponse(String accessToken, Object user, List<Tenant> stores, Boolean isStoreSelect) {
+            this.idToken = accessToken;
+            User userP = (User) user;
+            //this.id = userP.
+            this.username = userP.getUsername();
+            this.isStoreSelect = isStoreSelect;
+            //this.firstName = userP.getName();
+            this.authorities = String.join(",",userP.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet()));
+            this.tenants = String.join(",",stores.stream().map(Tenant::getTenantId).collect(Collectors.toSet()));
+        }
     }
 }
