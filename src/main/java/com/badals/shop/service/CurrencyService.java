726c809a663c9f943f09c293ac3f3c2915f4a254
Ebi <tmpmailebi@gmail.com>
2024-02-28 21:27:34 +0330

    fixed currency issue

--- src/main/java/com/badals/shop/service/CurrencyService.java
+++ src/main/java/com/badals/shop/service/CurrencyService.java
@@ -7,6 +7,7 @@ import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ import java.net.URL;
 import java.text.NumberFormat;
 import java.util.*;
 import java.util.stream.Collectors;
+
 import org.apache.commons.collections4.map.*;
 
 /**
@@ -31,38 +33,35 @@ import org.apache.commons.collections4.map.*;
 @Transactional
 public class CurrencyService {
 
-   public static final String BASE_CURRENCY_KEY = "base";
-   private static final Map<String, String> expiringMap = new PassiveExpiringMap<>(2000000);
+    public static final String BASE_CURRENCY_KEY = "base";
+    @Value("${currency.apikey}")
+    private static String API_KEY;
+    private static final Map<String, String> expiringMap = new PassiveExpiringMap<>(6 * 60 * 60 * 1000);
 
-   private final Logger log = LoggerFactory.getLogger(CurrencyService.class);
+    private final Logger log = LoggerFactory.getLogger(CurrencyService.class);
 
-   public static String convert(String amount, String from, String to) {
-      if(amount == null) return null;
-      String rate = expiringMap.get(from+to);
-      if(rate == null) {
-          try {
-                rate = getRate(from, to);
-                expiringMap.put(from+to, rate);
-            } catch (Exception e) {
-//                log.error("Error getting rate", e);
-                return "";
-          }
-      }
-      Locale locale = LocaleContextHolder.getLocale();
-      Currency currency = Currency.getInstance(locale);
-      NumberFormat format = NumberFormat.getNumberInstance();
-      format.setMaximumFractionDigits(2);
-      double price = Double.parseDouble(amount)*Double.parseDouble(rate);
-      String sPrice = format.format(price);
-      return sPrice;
-   }
+    public static String convert(String amount, String from, String to) {
+        if (amount == null) return null;
+        String rate = expiringMap.get(from + to);
+        if (rate == null) {
+            rate = getRate(from, to);
+            expiringMap.put(from + to, rate);
+        }
+        Locale locale = LocaleContextHolder.getLocale();
+        Currency currency = Currency.getInstance(locale);
+        NumberFormat format = NumberFormat.getNumberInstance();
+        format.setMaximumFractionDigits(2);
+        double price = Double.parseDouble(amount) * Double.parseDouble(rate);
+        return format.format(price);
+    }
 
-   @SneakyThrows
+    @SneakyThrows
     public static String getRate(String from, String to) {
-        String url_str = "https://api.exchangerate.host/convert?from="+from+"&to="+to;
+        String url_str = "https://api.apilayer.com/exchangerates_data/convert?from=" + from + "&to=" + to + "&amount=1";
 
         URL url = new URL(url_str);
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.setRequestProperty("apikey", API_KEY);
         request.connect();
 
         JsonParser jp = new JsonParser();
